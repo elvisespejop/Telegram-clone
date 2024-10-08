3f1157b35513715c117f3cd20f9d46065951ff33
Elvis <el.espejo@duocuc.cl>
2024-10-08 14:39:40 -0300

    isntall sdk streanchat

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,5 @@
 import { Slot } from "expo-router";
-
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 export default function RootLayout() {
-    return <Slot />;
+    return <GestureHandlerRootView style={{ flex: 1 }}><Slot /></GestureHandlerRootView>;
 }
